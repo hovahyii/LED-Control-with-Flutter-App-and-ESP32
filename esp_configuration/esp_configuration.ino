71f6a21cd87244d6caaf56daf83678aba59a1555
hovahyii <hovahyii@gmail.com>
2023-11-13 03:50:01 +0800

    updated control panel
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- esp_configuration/esp_configuration.ino
+++ esp_configuration/esp_configuration.ino
@@ -1,20 +1,64 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <LiquidCrystal_I2C.h>
 
 const char* ssid = "iemumpss.tech";
 const char* password = "hovah1234";
 
-const int ledRedPin = 12; // GPIO12 (D6) for the red LED
-const int ledGreenPin = 13; // GPIO13 (D7) for the green LED
-const int ledBluePin = 14;  // GPIO14 (D5) for the blue LED
+const int ledRedPin = 12;    // GPIO12 (D6) for the red LED
+const int ledGreenPin = 13;  // GPIO13 (D7) for the green LED
+const int ledBluePin = 14;   // GPIO14 (D5) for the blue LED
+
+const int lcdAddress = 0x27; // I2C address of your LCD
+const int lcdCols = 16;      // Number of columns on your LCD
+const int lcdRows = 2;       // Number of rows on your LCD
+
+LiquidCrystal_I2C lcd(lcdAddress, lcdCols, lcdRows);
 
 WebServer server(80);
+
+String lcdSentence = "";
+unsigned long lcdDisplayStartTime = 0;
+const unsigned long lcdDisplayDuration = 2000;  // 2 seconds
+const unsigned long ledDisplayDuration = 5000;  // 5 seconds for LED message display
+
+enum State {
+  IDLE,
+  LED_ON,
+  LED_OFF
+};
+
+State currentState = IDLE;
+unsigned long ledDisplayStartTime = 0;
+
+void displayLCD(const String& sentence) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(sentence.substring(0, 16));  // Display the first 16 characters on the first line
+
+  lcd.setCursor(0, 1);
+  lcd.print(sentence.substring(16));  // Display the remaining characters on the second line
+}
+
+void handleLED(State newState, int ledPin, const String& ledMessage) {
+  currentState = newState;
+
+  // Display LED message on the LCD
+  displayLCD(ledMessage);
+
+  server.send(200, "text/plain", ledMessage.c_str());
+  lcdSentence = lcdSentence;
+  lcdDisplayStartTime = millis();
+  ledDisplayStartTime = millis();  // Start the LED display timer
+}
 
 void setup() {
   Serial.begin(115200);
   pinMode(ledRedPin, OUTPUT);
   pinMode(ledGreenPin, OUTPUT);
   pinMode(ledBluePin, OUTPUT);
+
+  lcd.begin();
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -26,40 +70,61 @@ void setup() {
   Serial.print("ESP32 IP address: ");
   Serial.println(WiFi.localIP());
 
-
-  server.on("/red/on", HTTP_GET, [](){
-    digitalWrite(ledRedPin, HIGH); // Turn on the red LED
-    server.send(200, "text/plain", "Red LED is ON");
+  server.on("/red/on", HTTP_GET, []() {
+    digitalWrite(ledRedPin, HIGH);  // Turn on the red LED
+    handleLED(LED_ON, ledRedPin, "Red LED is ON");
   });
 
-  server.on("/red/off", HTTP_GET, [](){
-    digitalWrite(ledRedPin, LOW); // Turn off the red LED
-    server.send(200, "text/plain", "Red LED is OFF");
+  server.on("/red/off", HTTP_GET, []() {
+    digitalWrite(ledRedPin, LOW);  // Turn off the red LED
+    handleLED(LED_OFF, ledRedPin, "Red LED is OFF");
   });
 
-  server.on("/green/on", HTTP_GET, [](){
-    digitalWrite(ledGreenPin, HIGH); // Turn on the green LED
-    server.send(200, "text/plain", "Green LED is ON");
+  server.on("/green/on", HTTP_GET, []() {
+    digitalWrite(ledGreenPin, HIGH);  // Turn on the green LED
+    handleLED(LED_ON, ledGreenPin, "Green LED is ON");
   });
 
-  server.on("/green/off", HTTP_GET, [](){
-    digitalWrite(ledGreenPin, LOW); // Turn off the green LED
-    server.send(200, "text/plain", "Green LED is OFF");
+  server.on("/green/off", HTTP_GET, []() {
+    digitalWrite(ledGreenPin, LOW);  // Turn off the green LED
+    handleLED(LED_OFF, ledGreenPin, "Green LED is OFF");
   });
 
-  server.on("/blue/on", HTTP_GET, [](){
-    digitalWrite(ledBluePin, HIGH); // Turn on the blue LED
-    server.send(200, "text/plain", "Blue LED is ON");
+  server.on("/blue/on", HTTP_GET, []() {
+    digitalWrite(ledBluePin, HIGH);  // Turn on the blue LED
+    handleLED(LED_ON, ledBluePin, "Blue LED is ON");
   });
 
-  server.on("/blue/off", HTTP_GET, [](){
-    digitalWrite(ledBluePin, LOW); // Turn off the blue LED
-    server.send(200, "text/plain", "Blue LED is OFF");
+  server.on("/blue/off", HTTP_GET, []() {
+    digitalWrite(ledBluePin, LOW);  // Turn off the blue LED
+    handleLED(LED_OFF, ledBluePin, "Blue LED is OFF");
+  });
+
+  server.on("/lcd", HTTP_POST, []() {
+    lcdSentence = server.arg("sentence");
+    handleLED(IDLE, -1, lcdSentence);
+  });
+
+  server.onNotFound([]() {
+    server.send(404, "text/plain", "Not Found");
   });
 
   server.begin();
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+
+  // Display LCD sentence
+  if (currentState == IDLE && lcdSentence.length() > 0 && (currentTime - lcdDisplayStartTime < lcdDisplayDuration)) {
+    displayLCD(lcdSentence);
+  }
+
+  // Check if it's time to return to LCD display after showing LED message
+  if (currentState != IDLE && (currentTime - ledDisplayStartTime >= ledDisplayDuration)) {
+    displayLCD(lcdSentence);
+    currentState = IDLE;
+  }
+
   server.handleClient();
 }
--- lib/pages/control_panel_page.dart
+++ lib/pages/control_panel_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async'; // Import the dart:async library for Timer
 
 class ControlPanelPage extends StatefulWidget {
-  final String esp32Url = 'http://192.168.247.188'; // Replace with your ESP32 IP address: http://192.168.228.188
+  String esp32Url = ''; // Replace with your ESP32 IP address: http://192.168.228.188
 
   @override
   _ControlPanelPageState createState() => _ControlPanelPageState();
@@ -13,6 +14,11 @@ class _ControlPanelPageState extends State<ControlPanelPage> {
   bool redLedOn = false;
   bool greenLedOn = false;
   bool blueLedOn = false;
+  String lcdSentence = '';
+  Timer? lcdDisplayTimer;
+
+  TextEditingController esp32UrlController = TextEditingController();
+  TextEditingController lcdSentenceController = TextEditingController();
 
   Future<void> _sendControlRequest(int ledPin, bool ledState) async {
     String endpoint;
@@ -42,11 +48,20 @@ class _ControlPanelPageState extends State<ControlPanelPage> {
 
     try {
       final response = await http.get(
-        Uri.parse('${widget.esp32Url}/$endpoint'),
+        Uri.parse('http://${widget.esp32Url}/$endpoint'),
       );
       if (response.statusCode == 200) {
         // Request was successful, handle the response
         // You can update the UI or show a success message here
+        if (!ledState) {
+          // If LED is turned off, start a timer to display the LCD sentence for 2 seconds
+          lcdDisplayTimer?.cancel();
+          lcdDisplayTimer = Timer(Duration(seconds: 2), () {
+            setState(() {
+              lcdSentence = lcdSentenceController.text;
+            });
+          });
+        }
       } else {
         // Request failed, handle the error
         // You can update the UI or show an error message here
@@ -88,69 +103,145 @@ class _ControlPanelPageState extends State<ControlPanelPage> {
   }
 
   @override
+  void dispose() {
+    lcdDisplayTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 2,
-        mainAxisSpacing: 16,
-        crossAxisSpacing: 16,
-        padding: EdgeInsets.all(16),
-        children: <Widget>[
-          Container(
-            width: 150,
-            height: 150,
-            child: ElevatedButton.icon(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) => _getButtonColor(12, redLedOn),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    controller: esp32UrlController,
+                    decoration: InputDecoration(labelText: 'ESP32 URL'),
+                  ),
                 ),
               ),
-              onPressed: () => _sendControlRequest(12, !redLedOn),
-              icon: _getLedIcon(12, redLedOn),
-              label: Text(redLedOn ? 'Red LED On' : 'Red LED Off'),
-            ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    widget.esp32Url = esp32UrlController.text;
+                  });
+                },
+                child: Text('Submit'),
+              ),
+            ],
           ),
-          Container(
-            width: 150,
-            height: 150,
-            child: ElevatedButton.icon(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) => _getButtonColor(13, greenLedOn),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    controller: lcdSentenceController,
+                    decoration: InputDecoration(labelText: 'LCD Sentence'),
+                  ),
                 ),
               ),
-              onPressed: () => _sendControlRequest(13, !greenLedOn),
-              icon: _getLedIcon(13, greenLedOn),
-              label: Text(greenLedOn ? 'Green LED On' : 'Green LED Off'),
-            ),
+              ElevatedButton(
+                onPressed: () async {
+                  String lcdSentence = lcdSentenceController.text;
+                  try {
+                    final response = await http.post(
+                      Uri.parse('http://${widget.esp32Url}/lcd'),
+                      body: {'sentence': lcdSentence},
+                    );
+                      if (response.statusCode == 200) {
+        // Request was successful, handle the response
+        // You can update the UI or show a success message here
+        lcdDisplayTimer?.cancel(); // Cancel the timer before updating the LCD sentence
+        setState(() {
+          lcdSentence = lcdSentenceController.text;
+        });
+                    } else {
+                      // Request failed, handle the error
+                      // You can update the UI or show an error message here
+                    }
+                  } catch (e) {
+                    // Handle exceptions like network errors
+                    // You can display an error message or retry the request
+                    print('Error: $e');
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ],
           ),
-          Container(
-            width: 150,
-            height: 150,
-            child: ElevatedButton.icon(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) => _getButtonColor(14, blueLedOn),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              padding: EdgeInsets.all(16),
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: ElevatedButton.icon(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) => _getButtonColor(12, redLedOn),
+                      ),
+                    ),
+                    onPressed: () => _sendControlRequest(12, !redLedOn),
+                    icon: _getLedIcon(12, redLedOn),
+                    label: Text(redLedOn ? 'Red LED On' : 'Red LED Off'),
+                  ),
                 ),
-              ),
-              onPressed: () => _sendControlRequest(14, !blueLedOn),
-              icon: _getLedIcon(14, blueLedOn),
-              label: Text(blueLedOn ? 'Blue LED On' : 'Blue LED Off'),
-            ),
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            child: ElevatedButton.icon(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.grey),
-              ),
-              onPressed: () {
-                // Open the GitHub link when the button is pressed
-                launch('https://github.com/hovahyii/LED-Control-with-Flutter-App-and-ESP32');
-              },
-              icon: Icon(Icons.link, color: Colors.white, size: 60),
-              label: Text('About This Project'),
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: ElevatedButton.icon(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) => _getButtonColor(13, greenLedOn),
+                      ),
+                    ),
+                    onPressed: () => _sendControlRequest(13, !greenLedOn),
+                    icon: _getLedIcon(13, greenLedOn),
+                    label: Text(greenLedOn ? 'Green LED On' : 'Green LED Off'),
+                  ),
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: ElevatedButton.icon(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) => _getButtonColor(14, blueLedOn),
+                      ),
+                    ),
+                    onPressed: () => _sendControlRequest(14, !blueLedOn),
+                    icon: _getLedIcon(14, blueLedOn),
+                    label: Text(blueLedOn ? 'Blue LED On' : 'Blue LED Off'),
+                  ),
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: ElevatedButton.icon(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey),
+                    ),
+                    onPressed: () {
+                      // Open the GitHub link when the button is pressed
+                      launch('https://github.com/hovahyii/LED-Control-with-Flutter-App-and-ESP32');
+                    },
+                    icon: Icon(Icons.link, color: Colors.white, size: 60),
+                    label: Text('About This Project'),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
